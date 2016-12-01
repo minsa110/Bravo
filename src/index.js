ece76babfe1d12a63da099594ba660f35df38aaa
Daniel Snitkovskiy <Daniel@pc4d4uw.local>
2016-12-01 00:50:12 -0800

    Installed jQuery, added Add.js that can take zipcode and print listings to console.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import AddPage from './Add.js'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <AddPage />,
+//   document.getElementById('root')
+// );
