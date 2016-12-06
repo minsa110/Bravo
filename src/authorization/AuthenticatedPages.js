6c889838689a1862c2d3dc7046a5e04bd4c1cc68
Admin <rebeccata@D-173-250-183-215.dhcp4.washington.edu>
2016-12-05 17:12:52 -0800

    router issues

--- src/authorization/AuthenticatedPages.js
+++ src/authorization/AuthenticatedPages.js
@@ -1,5 +1,6 @@
 // Authenticated components
 import React from 'react';
+import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
 //import firebase from 'firebase';
 
 
@@ -7,7 +8,15 @@ var AuthenticatedPages = React.createClass({
 
     render() {
       return (
-        <p> Authenticated users only </p>
+        <div className="App">
+        <div className = "navbar">
+          <Link className="link" activeClassName='active' to="/">Home</Link>
+          <Link className="link" activeClassName='active' to="/page-2">Page2</Link>
+        </div>
+        <div className="children">
+          {this.props.children}
+        </div>
+        </div>
       )
     }
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
 import App from './authorization/App';
+import AuthenticatedPages from './authorization/AuthenticatedPages.js';
+import PageTwo from './authorization/pageTwo.js';
 import './index.css';
 
 import AddPage from './Add.js'
 
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <App />,
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={AuthenticatedPages}/>
+                <Route path="page-2" component={PageTwo}/>
+            </Route>
+        </Router>,
   document.getElementById('root')
 );
 
